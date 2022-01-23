aa78184578e88c84ef4249b4b29a45eb678d3b28
KeshavShelly <you@example.comkeshavshelly@gmail.com>
2022-01-23 17:17:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/header.js'
+import Hero from './components/hero.js'
+
+function App() {
+  return (
+    <body>
+      <div>
+        <Header />
+        <Hero />
+      </div>
+    </body>
+  );
+}
+
+export default App;
